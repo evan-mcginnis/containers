28bbefb8f967667e6e0e3625b6355154f78fbdbc
emcginnis <evan.mcginnis@gigamon.com>
2017-06-17 16:00:18 -0700

    checkpoint  Please enter the commit message for your changes. Lines starting

--- containers/statistics/getstats.py
+++ containers/statistics/getstats.py
@@ -2,6 +2,8 @@
 
 import requests
 import argparse
+import json
+from kafka import KafkaProducer
 
 parser = argparse.ArgumentParser(__file__, description="Fetch statistics from node")
 parser.add_argument("--target",
@@ -17,15 +19,28 @@ parser.add_argument("--password",
                     required=True,
                     help="password")
 
+def process_ports(value):
+    ports = value['ports']
+    for port in ports:
+        for key, value in port.iteritems():
+            print key, value
+
+
 def get_statistics(target, user, password):
-    url = "https://" + target + "/stats"
-    print("retrieve %s" % url)
-    response = requests.get(url, auth=(user, password))
-    print response.json
+    url = "https://" + target + "/api/v1/nodeCounters/all"
+    response = requests.get(url, auth=(user, password), verify=False)
+    payload = response.json()
+    for key, value in payload.iteritems():
+	#print key, value
+        if (key == 'ports'):
+            process_ports(value)
 
 
 if __name__ == "__main__":
+    producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v:json.dumps(v).encode('utf-8'))
     args = parser.parse_args()
+    requests.packages.urllib3.disable_warnings()
     get_statistics(args.target, args.user, args.password);
+
 
 
